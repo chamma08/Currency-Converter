5db4186b21f15cfb8831209a6e0db22be83bd1f6
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2024-01-27 18:46:58 +0530

    deleted package.lock

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Enable cors at the server side
 const corsOption = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:5000'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
